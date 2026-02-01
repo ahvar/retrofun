dd159a08da90283941bf8cf7af581a413d0fab8c
Arthur Vargas <arthurvargas@Arthurs-MacBook-Air.local>
2026-02-01 17:42:46 -0600

    initial commit

---
+++ db.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import DeclarativeBase
+
+
+# print(f"Database URL: {os.environ['DATABASE_URL']}")
+
+
+class Model(DeclarativeBase):
+    metadata = MetaData(
+        naming_convention={  # configure naming conventions for indexes and constraints
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_%(constraint_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
+
+
+load_dotenv()
+
+engine = create_engine(os.environ["DATABASE_URL"])
